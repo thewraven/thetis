d4d94c925ccac3bbf0cb1ae66536061c29b7110d
Juan Carlos García Martínez <juan.kklzone@gmail.com>
2017-07-27 15:53:45 -0500

    :construction: Ignoring the /metrics path

--- http_simple.go
+++ http_simple.go
@@ -11,28 +11,30 @@ type httpMetric struct {
 	collector *prometheus.CounterVec
 }
 
+//GetCollector returns the default collector to be registered
+//for the Handler
 func (h *httpMetric) GetCollector() prometheus.Collector {
 	responses := prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "http_status",
 		Help: "HTTP Status per request",
 	}, []string{"code", "route", "method"},
 	)
-	/*histogram: = prometheus.NewHistogram(prometheus.HistogramOpts {
-	    Name: "http_requests",
-	    Help: "HTTP Requests status and size per route.",
-	})*/
 	h.collector = responses
 	return responses
 }
 
 func (h *httpMetric) CreateHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		uri := r.RequestURI
+		//ignoring the metrics route by default
+		if uri == "/metrics" {
+			return
+		}
 		method := r.Method
 		status := "0"
 		if sw, ok := w.(*statusWriter); ok {
 			status = fmt.Sprint(sw.status)
 		}
-		uri := r.RequestURI
 		h.collector.WithLabelValues(status, uri, method).Inc()
 	}
 }
